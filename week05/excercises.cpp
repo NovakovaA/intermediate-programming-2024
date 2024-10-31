89a42422531e74aa8d69ad79d4cad025152d5478
anet <nowaneta@seznam.cz>
2024-10-31 20:45:39 +0100

    homework

--- week05/excercises.cpp
+++ week05/excercises.cpp
@@ -1,20 +1,66 @@
+#include <iostream>
 // TODO: Doplňte potřebné hlavičky
 
 // Vypište obdélník o zadaných parametrech
 // Vraťte 0 pokud vše v pořádku, -1 pokud nastala chyba
 int hollow_rect(int x, int y, int thickness) {
+    for(int i = 0; i < y; i++){
+        if(i < thickness || i >= y - thickness){
+            for(int h = 0; h < x; h++){
+                std::cout << '*';
+            }
+            std::cout << '\n';
+        }
+        else{
+            int s;
+            int p;
+            if(s <= thickness || s >= thickness){
+                for(s = 0; s < thickness; s++){
+                    std::cout << '*';
+                }
+                for(p = 0; p < x - thickness*2; p++){
+                    std::cout << ' ';
+                    }
+                for(s = 0; s < thickness; s++){
+                    std::cout << '*';
+                }
+                
+            std::cout << '\n';
+        }
+    }
+    }
     return -1;
+
 }
 
 // Vraťte a na n
 int power(int a, int n) {
+    int h = a;
+    for(int i = 1; i < n; i++){
+        h = h*a;
+    }
+    std::cout << h;
+    std::cout << '\n';
     return 0;
 }
 
 // Vraťte idx-té fibonacciho číslo
 int fibonacci(int idx) {
+    int x = 0;
+    int y = 1;
+    
+        int z;
+    for(int i = 0; i < idx; i++){
+        z = x + y;
+        x = y;
+        y = z;
+    }
+    std::cout << z;
     return 0;
 }
 
 int main() {
+    hollow_rect(12, 11, 3);
+    power(2, 3);
+    fibonacci(5);
 }
